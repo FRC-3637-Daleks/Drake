5bdad4187f034297cab652097c7d33e587d8d069
Harsha N <harshan.nalluri@gmail.com>
2019-02-11 17:42:47 -0500

    Working on Arm and Claw

--- src/main/include/Arm.h
+++ src/main/include/Arm.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "Robot.h";
 
+#define M_PI  3.14159265358979323846;
 
 class Arm {
 public:
 
-Arm(CANSparkMax *shoulderMotor, CANSparkMax *eblowMotor, WPI_TalonSRX *turretMotor, float  ARM_CAMERA);
+Arm(CANSparkMax *shoulderMotor, CANSparkMax *eblowMotor, WPI_TalonSRX *turretMotor, float armCamera);
 
 void ArmAngles(float x, float y);
 
 
 }
+
